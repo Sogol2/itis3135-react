5a0ee7f0462bddd7124905b3cbc1ea11ea62b260
Sogol2 <sogolmaghzian22@gmail.com>
2025-11-10 19:53:14 -0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,7 @@ function Footer(){
 }
 function Layout(){ return (<><Header/><main style={{maxWidth:900, margin:'0 auto', padding:'1rem 1.25rem'}}><Outlet/></main><Footer/></>) }
 
-function Home(){ return (<section style={{textAlign:'center'}}><h2>Welcome</h2><p>This is a simple React application structure.</p></section>) }
+function Home(){ return (<section style={{textAlign:'center'}}><h2>Welcome</h2><p>My name is Sogol, I am currently pursuing a Bachelor of Arts in Computer Science with a minor in Data Science at the University of North Carolina at Charlotte. I am passionate about technology and problem-solving, and I have developed strong technical skills in programming languages such as SQL, Java, JavaScript, Python, and C++. I enjoy spending my free time dancing, listening to the podcast, reading books and going to the gym.</p></section>) }
 function ProjectOverview(){ return (<section style={{textAlign:'center'}}><h2>Project Overview</h2><p>Same content as your normal site here.</p></section>) }
 function Introduction(){ return (<section style={{textAlign:'center'}}><h2>Introduction</h2><p>Your intro text here.</p></section>) }
 
